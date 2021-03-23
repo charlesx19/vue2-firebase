873921e07bdf2da8d6f9c729ecf7d46dcfb0f27c
charlesx19 <39717948+charlesx19@users.noreply.github.com>
2021-03-23 10:11:12 +0800

    add SignIn component

---
+++ src/db.js
@@ -0,0 +1,24 @@
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+
+// // Get a Firestore instance
+// export const db = firebase
+//   .initializeApp({ projectId: 'chrstest-927f2' })
+//   .firestore()
+
+// // Export types that exists in Firestore
+// // This is not always necessary, but it's used in other examples
+// const { Timestamp, GeoPoint } = firebase.firestore
+// export { Timestamp, GeoPoint }
+
+// // if using Firebase JS SDK < 5.8.0
+// db.settings({ timestampsInSnapshots: true })
+
+// Get a RTDB instance
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+
+export const db = firebase
+  .initializeApp({ databaseURL: 'https://chrstest-927f2-default-rtdb.firebaseio.com', apiKey: "AIzaSyDuk_kmqwZeyX_yCoBsxljyu6sHdVErops" })
+  .database()
